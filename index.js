6dcc4f0a3667b3d414848ce07fd3b45c5886bd72
zannatreya <zannatulbintabahar@yahoo.com>
2022-08-21 16:12:52 +0600

    Multiple reducers & combine multiple reducers

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
 
-// constants
+//product constants
 
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCTS = "ADD_PRODUCTS";
 
-// state
+//cart constants
+
+const GET_CART_ITEMS = "GET_CART_ITEMS";
+const ADD_CART_ITEMS = "ADD_CART_ITEMS";
+
+//product state
 
 const initialProductState={
     products:["salt","sugar"],
     numberOfProducts: 2,
 };
+//cart state
 
-// action
+const initialCartState={
+    cart:["sugar"],
+    numberOfProducts: 1,
+};
+
+//product action
 
 const getProductAction =()=>{
     return{
@@ -25,8 +36,21 @@ const addProductAction =(product)=>{
         payload: product,
     };
 };
+//cart action
 
-// reducer
+const getCartAction =()=>{
+    return{
+        type: GET_CART_ITEMS,
+    };
+};
+const addCartAction =(product)=>{
+    return{
+        type: ADD_CART_ITEMS,
+        payload: product,
+    };
+};
+
+//product reducer
 
 const productsReducer =(state=initialProductState,action)=>{
 switch (action.type) {
@@ -45,10 +69,35 @@ switch (action.type) {
     return state;
     }
 }
+//cart reducer
+
+const cartReducer =(state=initialCartState,action)=>{
+switch (action.type) {
+    case GET_CART_ITEMS:
+        return{
+            ...state,
+        };
+    case ADD_CART_ITEMS:
+        return{
+            cart: [...state.cart,action.payload],
+            numberOfProducts: state.numberOfProducts + 1,
+        };
+        
+
+    default:
+    return state;
+    }
+};
+
+const rootReducer = combineReducers({
+productR: productsReducer,
+cartR: cartReducer,
+});
 
 // store
 
-const store = createStore(productsReducer);
+// const store = createStore(productsReducer);
+const store = createStore(rootReducer);
 
 store.subscribe(()=>{
     console.log(store.getState());
@@ -57,3 +106,5 @@ store.subscribe(()=>{
 store.dispatch(getProductAction());
 store.dispatch(addProductAction("ata"));
 store.dispatch(addProductAction("moida"));
+store.dispatch(getCartAction());
+store.dispatch(addCartAction("suji"));
